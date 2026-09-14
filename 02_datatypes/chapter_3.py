fd29500bef7e95eedee3bb02fbea420e6fb438c4
aayush <ar0671362@gmail.com>
2026-09-14 22:59:22 +0530

    decoding

--- 02_datatypes/chapter_3.py
+++ 02_datatypes/chapter_3.py
@@ -1,8 +1,7 @@
-import sys
-ideal_temp = 95.5
-current_temp = 95.77
-
-print(f"ideal temp {ideal_temp}")
-print(f"current temp {current_temp}")
-print(f"Diff {current_temp - ideal_temp}")
-print(sys.float_info)
+customer_name = "Aayush"
+customer_order = "Chai"
+order_description = "Tulsi adarak chai"
+print(f"Order {customer_order} for {customer_name}")
+print(f"first word: {order_description[0:5:2]}")
+print(f"last word: {order_description[12:]}")
+print(f"reverse word: {order_description[::-1]}")
