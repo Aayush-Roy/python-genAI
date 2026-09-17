210b5063e25d9a55dfceda095fd2d03435facc37
aayush <ar0671362@gmail.com>
2026-09-17 21:55:14 +0530

    strings init

--- 02_datatypes/chapter_3.py
+++ 02_datatypes/chapter_3.py
@@ -4,4 +4,4 @@ order_description = "Tulsi adarak chai"
 print(f"Order {customer_order} for {customer_name}")
 print(f"first word: {order_description[0:5:2]}")
 print(f"last word: {order_description[12:]}")
-print(f"reverse word: {order_description[::-1]}")
+print(f"reverse word: {order_description[::1]}")
