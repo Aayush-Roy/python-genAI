250744d76b9f0622c6de61574989b911618adef2
aayush <ar0671362@gmail.com>
2026-09-18 22:31:24 +0530

    python strings questions

--- 02_datatypes/chapter_1.py
+++ 02_datatypes/chapter_1.py
@@ -1,12 +1,3 @@
-num1 = int(input("Enter num1: "))
-num2 = int(input("Enter num2: "))
-opr = input("Enter opr: ");
-
-if opr == '+':
-    print(f"Sum is {num1+num2}")
-elif opr == '-':
-    print(f"Difference is {num2-num1}")
-elif opr == '*':
-    print(f"Multiplies is {num1*num2}")
-else :
-    print("Invalid Operator!!")
+a = 1
+a-=5
+print(a)
