c190271f3c6ca0891320426ed529d669f710c1bd
aayush <ar0671362@gmail.com>
2026-09-19 22:41:42 +0530

    error handler in fn

--- number_guess/app.py
+++ number_guess/app.py
@@ -1,15 +1,14 @@
 import random
 random_num = random.randint(1,100)
 while True:
+    try:
+        number = int(input("Guess the number between 1 to 100 "))
     
-    number = int(input("Guess the number between 1 to 100 "))
-    if(number == random_num):
-        print(f"You guess the correct number")
-        break
-    elif number > random_num:
-        print("Too High")
-    elif number < random_num:
-        print("Too Low")
-    else:
-        print("Enter a valid Number")
-    
+        if number > random_num:
+            print("Too High")
+        elif number < random_num:
+            print("Too Low")
+        else:
+            print("congrats u guess the correct number")
+    except ValueError:
+        print("Enter a valid value")    
